a75e8429dd5de8143a0c1e03c96e15955c1cf468
Sabina Psuj <sabina.psuj@creativestyle.pl>
2019-02-26 08:35:36 +0100

    Added clickable version of piano

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 function playSound(e) {
     const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    
     sound.currentTime = 0;
     sound.play();
     key.classList.add('pressed');
+}
+
+function clickSound(e) {
+    const clicked = e.target;
+    const clickedSound = clicked.getAttribute('data-key');
+    const sound = document.querySelector(`audio[data-key="${clickedSound}"]`);
+
+    sound.currentTime = 0;
+    sound.play();
+    clicked.classList.add('pressed');
 }
 
 function removeTransition(e) {
@@ -29,7 +40,7 @@ window.addEventListener('keydown', playSound);
 
 let sounds = document.querySelectorAll('.sound');
 sounds.forEach(sound => sound.addEventListener('transitionend', removeTransition));
-
+sounds.forEach(sound => sound.addEventListener('click', clickSound));
 let hint = document.querySelector('.hint');
 hint.addEventListener('click', toggleHint);   
 
